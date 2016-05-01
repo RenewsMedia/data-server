4713d6de87e4f7752e4fd082876f25e4eadca06d
Onatolich <onatolich@gmail.com>
2016-05-02 00:28:00 +0300

    Update users search algorithm

--- api/v1/users.py
+++ api/v1/users.py
@@ -3,11 +3,21 @@ from api.v1 import app, db, make_path, gen_resp
 from api.v1.exceptions.NotFound import NotFound
 
 
+def prepare_query(query):
+    to_replace = ['.', ',']
+
+    for i in to_replace:
+        query.replace(to_replace[i], ' ')
+    query.replace('  ', ' ')
+
+    return query
+
+
 @app.route(make_path('/users'), methods=['GET'])
 def search():
     results = db.fetch("""
         PERFORM users_search({query})
-    """.format(query=request.form.query))
+    """.format(query=prepare_query(request.form.query)))
 
     if not len(results):
         raise NotFound
--- sql/functions/users_search.sql
+++ sql/functions/users_search.sql
@@ -1,13 +1,25 @@
 CREATE OR REPLACE FUNCTION users_search (
   s_query INTEGER
 ) RETURNS SETOF RECORD LANGUAGE plpgsql SECURITY DEFINER AS $$
+  DECLARE
+    parts VARCHAR[];
+    part VARCHAR;
+    c_match RECORD;
   BEGIN
---     TODO: make separation by spaces
-    RETURN QUERY
-      SELECT *
-      FROM "users"
-      WHERE "users"."login"   ~ $1 OR
-            "users"."name"    ~ $1 OR
-            "users"."surname" ~ $1;
+    parts := string_to_array(s_query, ' ');
+
+    FOREACH part IN ARRAY parts LOOP
+      FOR c_match IN
+        SELECT *
+        FROM "users"
+        WHERE "users"."login"   ~ $1 OR
+              "users"."name"    ~ $1 OR
+              "users"."surname" ~ $1
+      LOOP
+        RETURN NEXT c_match;
+      END LOOP;
+    END LOOP;
+
+    RETURN;
   END;
 $$;

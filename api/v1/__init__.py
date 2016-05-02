db1da7b540ea7c3ca501fb79493d17bbda394c67
Onatolich <onatolich@gmail.com>
2016-05-03 00:10:17 +0300

    Upd

--- api/v1/__init__.py
+++ api/v1/__init__.py
@@ -12,11 +12,6 @@ def make_path(path):
     return '/api/v1' + path
 
 
-# Import entities
-import api.v1.user
-import api.v1.users
-
-
 # List to sql array
 def list_to_sql(c_list):
     result = ''
@@ -36,3 +31,8 @@ def gen_resp(params, status_code=200):
 @app.errorhandler(Exception)
 def on_error(e):
     return gen_resp(e.to_dict(), e.status_code)
+
+
+# Import entities
+import api.v1.user
+import api.v1.users

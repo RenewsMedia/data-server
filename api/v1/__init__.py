2c9da542bc4108dbed8c7d67e448f3c53c887ab7
Onatolich <onatolich@gmail.com>
2016-05-05 14:08:55 +0300

    Update api files structure

--- api/v1/__init__.py
+++ api/v1/__init__.py
@@ -10,11 +10,11 @@ from api.v1.generic.AuthManager import AuthManager
 auth = AuthManager()
 
 # Import entities
-import api.v1.sign
-import api.v1.countries
-import api.v1.user
-import api.v1.users
-import api.v1.channel
-import api.v1.article
-import api.v1.contents
-import api.v1.tags
+import api.v1.entities.sign
+import api.v1.entities.countries
+import api.v1.entities.user
+import api.v1.entities.users
+import api.v1.entities.channel
+import api.v1.entities.article
+import api.v1.entities.contents
+import api.v1.entities.tags
